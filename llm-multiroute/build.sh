6313d98d62db2d92ef8a376371e6aa7e08dfc3eb
lu-gx <258742177+lu-gx@users.noreply.github.com>
2026-03-18 15:02:06 +0800

    update

--- llm-multiroute/build.sh
+++ llm-multiroute/build.sh
@@ -1,2 +1,2 @@
-  docker build --platform linux/arm64 -t darryl1975/llm-multiroute:latest .        
-  docker push darryl1975/llm-multiroute:latest 
+  docker build --platform linux/arm64 -t lugx/llm-multiroute:latest .        
+  docker push lugx/llm-multiroute:latest 
